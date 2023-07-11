18cb3fcaf19910d18a7fa56690c59e6c92d262fe
nc.exe_ <91869614+nc-exe@users.noreply.github.com>
2023-07-11 10:01:19 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+
+#include"format.h"
+#include"banner.h"
+#include"colortext.h"
+
+using namespace std;
+
+int main() {
+    int a,b,c,port,numeric_value,z;
+    string ip,command,range,output,domain,ip_r,interface,mac,distro;
+
+    banner();
+    options();
+
+    cout << "  Choose your option : ";
+    cin >> a;
+
+    if(a==1)
+    {
+        nmap(a);
+
+        cout << "Choose your option :";
+        cin >> b;
+
+        switch (b){
+            case 1:
+                nop1(a,b,ip,command);
+            break;
+
+            case 2:
+                nop2(a,b,ip,range,command);
+            break;
+
+            case 3:
+                nop3(a,b,port,ip,command);
+            break;
+
+            case 4:
+                nop4(a,b,ip,command);
+            break;
+
+            case 5:
+                nop5(a,b,c,output,domain,command);
+            break;
+
+            case 6:
+                nop6(a,b,numeric_value,ip,command);
+            break;
+
+            case 7:
+                nop7(a,b,ip_r,ip,command);
+            break;
+
+            case 8:
+                nop8(a,b,ip,command);
+            break;
+
+            default:
+                banner();
+                cout << "Invalid ARP option" << endl;
+                exit(0);
+        }
+
+    }
+    else if(a==2)
+    {
+        arp(a);
+
+        cout << "Choose your option :";
+        cin >> b;
+
+        switch (b)
+        {
+            case 1:
+                arp1(a,b);
+            break;
+
+            case 2:
+                arp2(a,b);
+            break;
+
+            case 3:
+                arp3(a,b,ip,command);
+            break;
+
+            case 4:
+                arp4(a,b,interface,mac,ip,command);
+            break;
+
+            default:
+                banner();
+                cout << "Invalid Input :-" << endl;
+                exit(0);
+        }
+    }
+    else if ( a == 6 )
+    {
+        ifconf(a);
+        enter();
+    }
+    else if ( a == 7 ){
+        upgrade(a,distro);
+    }
+    else
+    {
+        banner();
+        cout << "Invalid Input :-" << endl;
+        exit(0);
+    }
+    return 0;
+}
